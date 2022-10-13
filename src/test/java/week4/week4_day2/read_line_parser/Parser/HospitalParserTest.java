8097e0d8b8934315b900f2a3c6c508c0036ac539
chlalswns200 <chlalswns200@naver.com>
2022-10-13 13:15:32 +0900

    address 테스트 추가

--- src/test/java/week4/week4_day2/read_line_parser/Parser/HospitalParserTest.java
+++ src/test/java/week4/week4_day2/read_line_parser/Parser/HospitalParserTest.java
@@ -17,7 +17,7 @@ class HospitalParserTest {
 
         Assertions.assertEquals("A1120837", hospital.getId());
 
-        //주소 파싱 추가
+        //주소 파싱 테스트 추가
         String address = "서울특별시 금천구 벚꽃로 286 삼성리더스타워 111~114호 (가산동)";
 
         Assertions.assertEquals(address,hospital.getAddress());
