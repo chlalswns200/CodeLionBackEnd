ef1d2379c3116a779b2d788e364f0a41c201c806
chlalswns200 <chlalswns200@naver.com>
2022-10-14 09:50:32 +0900

    삽입 정렬 swap까지

--- src/main/java/week4/week4_day4/InsertionSort.java
+++ src/main/java/week4/week4_day4/InsertionSort.java
@@ -6,9 +6,9 @@ public class InsertionSort {
 
     public int[] sort(int[] arr) {
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] < arr[j]) {
                     int tmp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = tmp;
