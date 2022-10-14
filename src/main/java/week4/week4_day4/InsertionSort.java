8026ed8347c58748fc48a4a8eebe957c6e359157
chlalswns200 <chlalswns200@naver.com>
2022-10-14 10:49:57 +0900

    삽입 정렬 재귀 추가

--- src/main/java/week4/week4_day4/InsertionSort.java
+++ src/main/java/week4/week4_day4/InsertionSort.java
@@ -18,12 +18,32 @@ public class InsertionSort {
 
         return arr;
     }
+
+    public int[] sortByRecursive(int[] arr, int i) {
+
+        if(i == arr.length) return arr;
+
+        for (int j = 0; j > 0; j--) {
+            if (arr[j] > arr[j - 1]) {
+                int tmp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = tmp;
+            }
+        }
+        sortByRecursive(arr, i + 1);
+        return arr;
+    }
+
     public static void main(String[] args) {
 
-        int[] ar = new int[]{8, 5, 6, 2, 4};
+        int[] ar = new int[]{8, 5, 6, 2, 4,7,10,9,1};
         InsertionSort is = new InsertionSort();
+
         int[] sort = is.sort(ar);
         System.out.println(Arrays.toString(sort));
+
+        int[] ints = is.sortByRecursive(ar, 1);
+        System.out.println(Arrays.toString(ints));
 
     }
 }
