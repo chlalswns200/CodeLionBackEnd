d9aac88e7a3c56bfd73becec40ca19cbb535d3c0
chlalswns200 <chlalswns200@naver.com>
2022-10-17 09:08:27 +0900

    별 찍기1

---
+++ src/main/java/week5/week5_day1/RightTriangle.java
@@ -0,0 +1,15 @@
+package week5.week5_day1;
+
+public class RightTriangle {
+
+    public static void main(String[] args) {
+
+        System.out.println("*");
+
+        System.out.println("**");
+
+        System.out.println("***");
+
+        System.out.println("****");
+    }
+}
