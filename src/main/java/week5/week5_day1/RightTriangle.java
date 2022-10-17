a59f505c810f06df19813027db83076c61d7fb99
chlalswns200 <chlalswns200@naver.com>
2022-10-17 09:21:45 +0900

    별 찍기 2

--- src/main/java/week5/week5_day1/RightTriangle.java
+++ src/main/java/week5/week5_day1/RightTriangle.java
@@ -1,15 +1,25 @@
 package week5.week5_day1;
 
+import java.util.Scanner;
+
 public class RightTriangle {
+
+    private void drwaTriangle(int n) {
+        for (int i=1; i <= n; i++) {
+
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 
     public static void main(String[] args) {
 
-        System.out.println("*");
+        Scanner sc = new Scanner(System.in);
+        RightTriangle rt = new RightTriangle();
+        int n = sc.nextInt();
+        rt.drwaTriangle(n);
 
-        System.out.println("**");
-
-        System.out.println("***");
-
-        System.out.println("****");
     }
 }
