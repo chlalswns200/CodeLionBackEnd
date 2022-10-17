cbfca8ae74debe9a1e74ea185b88f39b770cbe53
chlalswns200 <chlalswns200@naver.com>
2022-10-17 09:57:04 +0900

    별 찍기3 피라미드

--- src/main/java/week5/week5_day1/RightTriangle.java
+++ src/main/java/week5/week5_day1/RightTriangle.java
@@ -4,11 +4,20 @@ import java.util.Scanner;
 
 public class RightTriangle {
 
+    private String letter = "*";
+
+    public RightTriangle() {
+    }
+
+    public RightTriangle(String letter) {
+        this.letter = letter;
+    }
+
     private void drwaTriangle1(int n) {
         for (int i=1; i <= n; i++) {
 
             for (int j = 0; j < i; j++) {
-                System.out.print("*");
+                System.out.print(this.letter);
             }
             System.out.println("");
         }
@@ -17,13 +26,13 @@ public class RightTriangle {
     private void drwaTriangle2(int n) {
         for (int i = 0; i < n; i++) {
             System.out.print(" ".repeat(n-i-1));
-            System.out.println("*".repeat(2*i+1));
+            System.out.println(this.letter.repeat(2*i+1));
         }
     }
 
     public static void main(String[] args) {
 
-        RightTriangle rt = new RightTriangle();
-        rt.drwaTriangle2(7);
+        RightTriangle rt = new RightTriangle("@");
+        rt.drwaTriangle2(5);
     }
 }
