c38a72a500c294537ee937564109b162cb1bf5d8
chlalswns200 <chlalswns200@naver.com>
2022-10-17 10:17:52 +0900

    별 찍기4 마름모

--- src/main/java/week5/week5_day1/RightTriangle.java
+++ src/main/java/week5/week5_day1/RightTriangle.java
@@ -26,13 +26,26 @@ public class RightTriangle {
     private void drwaTriangle2(int n) {
         for (int i = 0; i < n; i++) {
             System.out.print(" ".repeat(n-i-1));
-            System.out.println(this.letter.repeat(2*i+1));
+            System.out.println((this.letter).repeat(2*i+1));
+
+        }
+    }
+
+
+    private void drwaRhombus(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(" ".repeat(n-i-1));
+            System.out.println((this.letter).repeat(2*i+1));
+        }
+        for (int i = n-1; i > 0; i--) {
+            System.out.print(" ".repeat(n-i));
+            System.out.println((this.letter).repeat(2*i-1));
         }
     }
 
     public static void main(String[] args) {
 
-        RightTriangle rt = new RightTriangle("@");
-        rt.drwaTriangle2(5);
+        RightTriangle rt = new RightTriangle();
+        rt.drwaRhombus(5);
     }
 }
