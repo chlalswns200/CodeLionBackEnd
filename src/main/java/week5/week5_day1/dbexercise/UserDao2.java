82c8a48d8524f7dc3674a8004a06daa03388553e
chlalswns200 <chlalswns200@naver.com>
2022-10-17 13:42:21 +0900

    select 추가

--- src/main/java/week5/week5_day1/dbexercise/UserDao2.java
+++ src/main/java/week5/week5_day1/dbexercise/UserDao2.java
@@ -1,9 +1,8 @@
 package week5.week5_day1.dbexercise;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import week5.week5_day1.dbexercise.domain.User;
+
+import java.sql.*;
 import java.util.Map;
 
 //환경 변수를 통해 id name password를 따로 저장하고 하는 방법
@@ -29,8 +28,41 @@ public class UserDao2 {
         ps.close();
         conn.close();
     }
-    public static void main(String[] args) throws SQLException {
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(
+                dbHost, dbUser, dbPassword);
+
+        PreparedStatement ps = conn.prepareStatement(
+                "select id, name ,password FROM users WHERE id = ?");
+        ps.setString(1, id);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+
+        User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        conn.close();
+
+        return user;
+
+    }
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
         UserDao2 userDao = new UserDao2();
-        userDao.add();
+       // userDao.add();
+
+        User user = userDao.get("1");
+        System.out.println("user.getName() = " + user.getName());
+
+
     }
 }
