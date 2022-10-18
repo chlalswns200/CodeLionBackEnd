eddeb412bc9f05d81b593d37b3f960890d518f62
chlalswns200 <chlalswns200@naver.com>
2022-10-18 14:50:28 +0900

    interface 추가

--- src/test/java/week5/week5_day1/dbexercise/UserDao2Test.java
+++ src/test/java/week5/week5_day1/dbexercise/UserDao2Test.java
@@ -2,7 +2,7 @@ package week5.week5_day1.dbexercise;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import week5.week5_day1.dbexercise.dao.AWSUse4rDaoImpl;
+import week5.week5_day1.dbexercise.dao.*;
 import week5.week5_day1.dbexercise.domain.User;
 
 import java.sql.SQLException;
@@ -11,11 +11,11 @@ class UserDao2Test {
 
     @Test
     void addAndSelect() throws SQLException, ClassNotFoundException {
-        AWSUse4rDaoImpl userDao = new AWSUse4rDaoImpl();
-        User user = new User("12", "Eternity", "1123");
-        userDao.add(user);
+        UserDao2 userDao = new UserDao2();
+        User user = new User("15", "Eternity", "1123");
+        //userDao.add(user);
 
-        User findUser = userDao.findById("12");
+        User findUser = userDao.findById("15");
         Assertions.assertEquals("Eternity", findUser.getName());
     }
 
