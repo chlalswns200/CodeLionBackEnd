baeda1a8fc089d92088a73848a0d441715019bec
chlalswns200 <chlalswns200@naver.com>
2022-10-18 10:30:28 +0900

    사각형 2 추가

--- src/main/java/week5/week5_day2/Square.java
+++ src/main/java/week5/week5_day2/Square.java
@@ -20,12 +20,35 @@ public class Square {
         }
     }
 
+    public void printSquare2(int n) {
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || i == n - 1) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            } else {
+                for (int k = 0; k < n; k++) {
+                    if (k == 0 || k == n - 1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+
+                System.out.println();
+            }
+        }
+    }
+
+
+
     public static void main(String[] args) {
 
         Square sq = new Square();
-        sq.printSquare(4);
-
-        sq.printRectangle(5,3);
+        //sq.printSquare(4);
+        //sq.printRectangle(5,3);
+        sq.printSquare2(3);
 
     }
 }
