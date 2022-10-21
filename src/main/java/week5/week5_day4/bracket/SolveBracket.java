73e5ccca0a0da50ce94e13b64bde653489a53260
chlalswns200 <chlalswns200@naver.com>
2022-10-21 10:44:42 +0900

    괄호 풀기 split, stack 추가

--- src/main/java/week5/week5_day4/bracket/SolveBracket.java
+++ src/main/java/week5/week5_day4/bracket/SolveBracket.java
@@ -1,6 +1,9 @@
 package week5.week5_day4.bracket;
 
+import java.util.Stack;
+
 public class SolveBracket {
+    //indexOf 함수
     boolean solution(String s) {
 
         while (s.indexOf("()") >= 0) {
@@ -9,7 +12,27 @@ public class SolveBracket {
 
         return s.length() == 0;
     }
-    public static void main(String[] args) {
 
+    //split 함수
+    boolean solution2(String s) {
+        while (s.indexOf("()")>=0) {
+            String[] split = s.split("\\(\\)");
+            s = String.join("",split);
+        }
+        return s.length() == 0;
+    }
+
+    //stack으로 풀기
+    boolean solution3(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if ('(' == (s.charAt(i))) {
+                st.push(s.charAt(i));
+            } else if (')' == (s.charAt(i))) {
+                if(st.empty()) return false;
+                st.pop();
+            }
+        }
+        return st.empty();
     }
 }
--- src/test/java/week5/week5_day4/bracket/SolveBracketTest.java
+++ src/test/java/week5/week5_day4/bracket/SolveBracketTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SolveBracketTest {
 
     @Test
-    @DisplayName("괄호가 잘 풀리는지")
+    @DisplayName("괄호가 잘 풀리는지 - indexOf 함수")
     void bracket() {
         SolveBracket sb = new SolveBracket();
         assertTrue(sb.solution("()()"));
@@ -16,8 +16,29 @@ class SolveBracketTest {
         assertTrue(sb.solution("(())()"));
         assertFalse(sb.solution(")()("));
         assertFalse(sb.solution( "((((((((((((((((((((((((((((((((((((((((((((((((())))))))))))))))))))))))))))))))))))))))))))))))))))))"));
+    }
 
+    @Test
+    @DisplayName("괄호가 잘 풀리는지2 - split 함수")
+    void bracket2() {
+        SolveBracket sb = new SolveBracket();
+        assertTrue(sb.solution2("()()"));
+        assertTrue(sb.solution2("(()())()"));
+        assertTrue(sb.solution2("(())()"));
+        assertFalse(sb.solution2(")()("));
+        assertFalse(sb.solution2( "((((((((((((((((((((((((((((((((((((((((((((((((())))))))))))))))))))))))))))))))))))))))))))))))))))))"));
 
+    }
+
+    @Test
+    @DisplayName("괄호가 잘 풀리는지3 - stack")
+    void bracket3() {
+        SolveBracket sb = new SolveBracket();
+        assertTrue(sb.solution2("()()"));
+        assertTrue(sb.solution2("(()())()"));
+        assertTrue(sb.solution2("(())()"));
+        assertFalse(sb.solution2(")()("));
+        assertFalse(sb.solution2( "((((((((((((((((((((((((((((((((((((((((((((((((())))))))))))))))))))))))))))))))))))))))))))))))))))))"));
     }
 
 }
