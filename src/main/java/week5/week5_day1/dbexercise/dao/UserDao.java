2616100f619a63a757d1872a5c0b58e47946bd40
chlalswns200 <chlalswns200@naver.com>
2022-10-24 15:14:57 +0900

    jdbcTemplate 적용

--- src/main/java/week5/week5_day1/dbexercise/dao/UserDao.java
+++ src/main/java/week5/week5_day1/dbexercise/dao/UserDao.java
@@ -1,6 +1,8 @@
 package week5.week5_day1.dbexercise.dao;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import week5.week5_day1.dbexercise.domain.User;
 
 import javax.sql.DataSource;
@@ -11,98 +13,39 @@ import java.sql.*;
 public class UserDao {
 
     private final DataSource dataSource;
-    private final JdbcContext jdbcContext;
+    private final JdbcTemplate jdbcTemplate;
 
     public UserDao(DataSource dataSource) {
         this.dataSource = dataSource;
-        this.jdbcContext = new JdbcContext(dataSource);
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void executeSql(String sql) throws SQLException {
-        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+    public void add(final User user) throws SQLException {
+        this.jdbcTemplate.update("INSERT INTO users(id,name,password) values(?,?,?)",
+                user.getId(),
+                user.getName(),
+                user.getPassword()
+        );
+    }
 
+    public User findById(String id) {
+        String sql = "select * from users where id = ?";
+        RowMapper<User> rowMapper = new RowMapper<User>() {
             @Override
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                return c.prepareStatement(sql);
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+                return user;
             }
-        });
-    }
+        };
 
-    public void add(User user) throws SQLException {
-        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-            @Override
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement(
-                        "INSERT INTO users(id,name,password) values(?,?,?)"
-                );
-                ps.setString(1,user.getId());
-                ps.setString(2,user.getName());
-                ps.setString(3,user.getPassword());
-                return ps;
-            }
-        });
-    }
-
-    public User findById(String id) throws SQLException {
-
-        Connection conn = dataSource.getConnection();
-        PreparedStatement ps = conn.prepareStatement(
-                "select id, name ,password FROM users WHERE id = ?");
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
-        User user = null;
-        if (rs.next()) {
-            user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
-        }
-
-        rs.close();
-        ps.close();
-        conn.close();
-
-        if(user ==null) throw new EmptyResultDataAccessException(1);
-        return user;
-
+        return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
 
     public void deleteAll() throws SQLException {
-        executeSql(("delete from users"));
+        jdbcTemplate.update("delete from users");
     }
 
     public int getCount() {
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            ps = conn.prepareStatement(
-                    "SELECT count(*) from users");
-            rs = ps.executeQuery();
-            rs.next();
-            int count = rs.getInt(1);
-            return count;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
+        return this.jdbcTemplate.queryForObject("select count(*) from users;", Integer.class);
     }
 }
