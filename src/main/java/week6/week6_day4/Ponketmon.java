147fe957d08ee0393a15eb0c9a136dffaa845641
chlalswns200 <chlalswns200@naver.com>
2022-10-28 09:40:39 +0900

    Ponketmon set으로 풀기 추가

--- src/main/java/week6/week6_day4/Ponketmon.java
+++ src/main/java/week6/week6_day4/Ponketmon.java
@@ -1,11 +1,14 @@
 package week6.week6_day4;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Ponketmon {
 
-    public int solution(int[] nums) {
+    //List로 풀기
+    public int solutionA(int[] nums) {
         int answer = 0;
         int maxCount = nums.length/2;
         List<Integer> intList = new ArrayList<>();
@@ -28,11 +31,31 @@ public class Ponketmon {
         return intList.size();
     }
 
+    //Set으로 풀기
+    public int solutionB(int[] nums) {
+
+        int answer = 0;
+        int maxCount = nums.length/2;
+        Set<Integer> intSet = new HashSet<>();
+
+        for (int i : nums) {
+            intSet.add(i);
+            if (intSet.size() == maxCount) {
+                return intSet.size();
+            }
+        }
+
+        return intSet.size();
+    }
+
     public static void main(String[] args) {
 
         int ar[] = {3,3,3,2,2,2};
         Ponketmon pk = new Ponketmon();
-        int solution = pk.solution(ar);
-        System.out.println("solution = " + solution);
+        int solutionA = pk.solutionA(ar);
+        int solutionB = pk.solutionB(ar);
+
+        System.out.println("solutionA = " + solutionA);
+        System.out.println("solutionB = " + solutionB);
     }
 }
