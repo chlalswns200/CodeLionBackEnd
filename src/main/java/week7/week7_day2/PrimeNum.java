33629086375882fa286574a949763f0b79668b2c
chlalswns200 <chlalswns200@naver.com>
2022-11-01 10:18:27 +0900

    콜백 적용 1

--- src/main/java/week7/week7_day2/PrimeNum.java
+++ src/main/java/week7/week7_day2/PrimeNum.java
@@ -1,13 +1,14 @@
 package week7.week7_day2;
 
 public class PrimeNum {
+
     public int solution(String numbers) {
         int answer = 0;
         return answer;
     }
 
     public boolean isPrime(int num) {
-        for (int i = 2; i*i <=num ; i++) {
+        for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) {
                 return false;
             }
@@ -17,12 +18,9 @@ public class PrimeNum {
 
     public static void main(String[] args) {
 
-        int[] arr = {8,13,17,19,23};
-        String str = "011";
+        int[] arr = {8, 13, 17, 19, 23};
+        String str = "17";
         PrimeNum pn = new PrimeNum();
-        for (int i : arr) {
-            System.out.println(pn.isPrime(i));
-        }
 
     }
 }
