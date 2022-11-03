3d71c67f33a862c77ed7bebe9096e9efb2ef4232
chlalswns200 <chlalswns200@naver.com>
2022-11-03 10:02:44 +0900

    에라토스테네스의 체

--- src/main/java/week7/week7_day4/MultipleOf.java
+++ src/main/java/week7/week7_day4/MultipleOf.java
@@ -1,15 +1,20 @@
 package week7.week7_day4;
 
 
+import java.util.Arrays;
+
 public class MultipleOf {
 
     public static void main(String[] args) {
 
-        long num =2;
-        for (int i = 2; i < 100; i++) {
-            System.out.println( i+" "+num);
-            num+=2;
+        int N = 50;
+        int[] nums = new int[N - 1];
+        boolean[] checks = new boolean[nums.length];
+        Arrays.fill(checks,true);
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i+2;
         }
+        System.out.println(Arrays.toString(nums));
     }
 
 }
