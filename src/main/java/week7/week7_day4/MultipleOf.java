757832e13c52191daf7f5cea78c8889da3777f9c
chlalswns200 <chlalswns200@naver.com>
2022-11-03 09:29:59 +0900

    multipleOf가 100까지

---
+++ src/main/java/week7/week7_day4/MultipleOf.java
@@ -0,0 +1,15 @@
+package week7.week7_day4;
+
+
+public class MultipleOf {
+
+    public static void main(String[] args) {
+
+        long num =2;
+        for (int i = 2; i < 100; i++) {
+            System.out.println( i+" "+num);
+            num*=2;
+        }
+    }
+
+}
