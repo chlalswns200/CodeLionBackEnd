bd83936d1a078ac587b66599c8fd2f56ea3d3415
chlalswns200 <chlalswns200@naver.com>
2022-11-08 10:35:11 +0900

    이진 탐색2

--- src/main/java/week8/week8_day1/BinarySearch.java
+++ src/main/java/week8/week8_day1/BinarySearch.java
@@ -7,23 +7,17 @@ public class BinarySearch {
         int targetNum = 7;
         // 중간값 찾기
         int midNumIdx = nums.length/2;
+        int startIdx = 0;
+        int endIdx = nums.length-1;
 
-        int startIdx;
-        int endIdx;
         // 인덱스(시작점,끝점) 옮기기
         if (nums[midNumIdx] < targetNum) {
-            startIdx = midNumIdx + 1;
-            endIdx = nums.length - 1;
-        } else {
-            startIdx = 0;
             endIdx = midNumIdx - 1;
+        } else if(nums[midNumIdx]>targetNum) {
+            startIdx = midNumIdx+1;
+        } else {
+            System.out.println(midNumIdx);
         }
-        midNumIdx = nums[(startIdx+endIdx)/2];
-
-
-        // 같은지 비교하기
-        if (nums[startIdx] == targetNum) System.out.println(startIdx);
-        if(nums[endIdx]==targetNum) System.out.println(endIdx);
 
     }
 }
