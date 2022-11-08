e9bd3ef7bab227d83e58d79accf1e2c7e9e3fafc
chlalswns200 <chlalswns200@naver.com>
2022-11-08 11:07:01 +0900

    이진 탐색 완료

--- src/main/java/week8/week8_day1/BinarySearch.java
+++ src/main/java/week8/week8_day1/BinarySearch.java
@@ -2,22 +2,31 @@ package week8.week8_day1;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] nums = new int[]{1,2,3,4,5,6,7,8,9,10,11};
-
         int targetNum = 7;
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
         // 중간값 찾기
-        int midNumIdx = nums.length/2;
         int startIdx = 0;
-        int endIdx = nums.length-1;
+        int endIdx = nums.length - 1;
+        int targetIdx = -1;
 
-        // 인덱스(시작점,끝점) 옮기기
-        if (nums[midNumIdx] < targetNum) {
-            endIdx = midNumIdx - 1;
-        } else if(nums[midNumIdx]>targetNum) {
-            startIdx = midNumIdx+1;
-        } else {
-            System.out.println(midNumIdx);
+        while (startIdx <= endIdx) {
+            int midIdx = (startIdx + endIdx) / 2;
+            int midValue = nums[midIdx];
+            // 인덱스(시작점, 끝점) 옮기기
+            if (midValue > targetNum) { //  1 -(3)- 6 --- 11
+                endIdx = midIdx - 1;
+            } else if (midValue < targetNum) { //  1 --- 6 --(8)- 11
+                startIdx = midIdx + 1;
+            } else {
+                // 같은경우 = 답
+                // 같은지 비교하기
+                System.out.println(midIdx);
+                targetIdx = midIdx;
+                break;
+            }
         }
-
     }
+
 }
+
+
