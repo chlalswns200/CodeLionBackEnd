6f81d31f51e0dd13bf3da2f0fcafc9a2ea9443e4
chlalswns200 <chlalswns200@naver.com>
2022-11-09 10:05:55 +0900

    선택 정렬 내림차순

--- src/main/java/week8/week8_day2/SelectionSort.java
+++ src/main/java/week8/week8_day2/SelectionSort.java
@@ -7,17 +7,17 @@ public class SelectionSort {
         int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
 
         for (int i = 0; i < arr.length; i++) {
-            int min = arr[i];
-            int minIdx=i;
+            int max = arr[i];
+            int maxIdx=i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < min) {
-                    min = arr[j];
-                    minIdx = j;
+                if (arr[j] > max) {
+                    max = arr[j];
+                    maxIdx = j;
                 }
             }
             int tmp = arr[i];
-            arr[i] = arr[minIdx];
-            arr[minIdx] = tmp;
+            arr[i] = arr[maxIdx];
+            arr[maxIdx] = tmp;
         }
 
         System.out.println(Arrays.toString(arr));
