40b5cca8ae762acc16de543d11d67cad3ef6a9a0
chlalswns200 <chlalswns200@naver.com>
2022-11-11 10:27:19 +0900

    비트연산 후 이진수

--- src/main/java/week8/week8_day4/Binary.java
+++ src/main/java/week8/week8_day4/Binary.java
@@ -1,23 +1,37 @@
 package week8.week8_day4;
 
+
 public class Binary {
+
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+        answer = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String s = Integer.toBinaryString(arr1[i] | arr2[i]);
+            if (s.length() < n) {
+                s = "0".repeat(n-s.length()) + s;
+            }
+            String replace = s.replace("1", "#");
+            String replace1 = replace.replace("0", " ");
+            answer[i] = replace1;
+        }
+
+        return answer;
+    }
     public static void main(String[] args) {
 
-        int n = 5;
-        int[] arr1 = new int[]{9, 20, 28, 18, 11};
+        int n = 6;
+        int[] arr1 = new int[]{46, 33, 33 ,22, 31, 50};
+        int[] arr2 = new int[]{27 ,56, 19, 14, 14, 10};
 
+        Binary br = new Binary();
+        String[] solution = br.solution(n, arr1, arr2);
 
-        for (int i : arr1) {
-            String zero = "0";
-            String result = "";
-            for(int j=i; j>0; j/=2) {
-                result = String.valueOf(j%2) + result;
-            }
-            if (n - result.length() > 0) {
-                zero.repeat(n-result.length());
-                result = zero+result;
-            }
-            System.out.println(result);
+        for (String s : solution) {
+            System.out.println(s);
         }
+
+
     }
 }
