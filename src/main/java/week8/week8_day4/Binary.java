8f54b7c459df54b94a7d86100a2a2582b42df6dc
chlalswns200 <chlalswns200@naver.com>
2022-11-11 09:57:20 +0900

    자리수 해결

--- src/main/java/week8/week8_day4/Binary.java
+++ src/main/java/week8/week8_day4/Binary.java
@@ -3,13 +3,21 @@ package week8.week8_day4;
 public class Binary {
     public static void main(String[] args) {
 
-        int n =12;
-        String result = ""; //출력할 값
+        int n = 5;
+        int[] arr1 = new int[]{9, 20, 28, 18, 11};
 
-        for(int i=n; i>0; i/=2) {
-            result = String.valueOf(i%2) + result;
+
+        for (int i : arr1) {
+            String zero = "0";
+            String result = "";
+            for(int j=i; j>0; j/=2) {
+                result = String.valueOf(j%2) + result;
+            }
+            if (n - result.length() > 0) {
+                zero.repeat(n-result.length());
+                result = zero+result;
+            }
+            System.out.println(result);
         }
-
-        System.out.println(result);
     }
 }
