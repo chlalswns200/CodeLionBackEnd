fa51605b2306041a24f2fb92a035a8c301b07d87
chlalswns200 <chlalswns200@naver.com>
2022-11-14 10:50:01 +0900

    체 완성

--- src/main/java/week9/Eratos.java
+++ src/main/java/week9/Eratos.java
@@ -12,9 +12,14 @@ public class Eratos {
             arr[i] = i+2;
         }
 
-        for (int i = 2; i < arr.length; i+=2) {
-            arr[i] = 0;
+        for (int i = 0; i * i <= N; i++) {
+            if (arr[i] != 0) {
+                for (int j = i*2+2; j < arr.length; j+=arr[i]) {
+                    arr[j]=0;
+                }
+            }
         }
+
         System.out.println(Arrays.toString(arr));
 
     }
