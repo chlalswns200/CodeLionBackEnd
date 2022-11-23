2e8354ffae2fad0614ab7282b316b06bd7a8d21c
chlalswns200 <chlalswns200@naver.com>
2022-11-23 10:41:34 +0900

    재귀없이 - 피보나치

--- src/main/java/week10/week10_day2/Fibonacci2.java
+++ src/main/java/week10/week10_day2/Fibonacci2.java
@@ -1,13 +1,29 @@
 package week10.week10_day2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fibonacci2 {
     static int fibonacci(int n) {
         if (n == 1 || n == 2) return 1;
         else return fibonacci(n - 1) + fibonacci(n - 2);
     }
+    
+    static int fib(int n) {
+        if(n==1|n==2) return 1;
+        
+        List<Integer> fibonacciNums = new ArrayList<>();
+        fibonacciNums.add(1);
+        fibonacciNums.add(1);
+        for (int j = 2; j < n; j++) {
+            fibonacciNums.add(fibonacciNums.get(j - 1) + fibonacciNums.get(j - 2));
+        }
+        return fibonacciNums.get(n-1);
+    }
 
     public static void main(String[] args) {
-        int fibo = fibonacci(10);
-        System.out.println("fibo = " + fibo);
+        int fib = fib(5);
+        System.out.println("fib = " + fib);
+        
     }
 }
