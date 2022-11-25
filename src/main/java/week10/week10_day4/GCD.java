dd1ba4f9f872bc63b9a306a0a3e34c793ab823c2
chlalswns200 <chlalswns200@naver.com>
2022-11-25 10:04:36 +0900

    최대공약수 재귀없이

--- src/main/java/week10/week10_day4/GCD.java
+++ src/main/java/week10/week10_day4/GCD.java
@@ -8,9 +8,20 @@ public class GCD {
         }
         return getGCD(num2, num1%num2);
     }
+    public static int getGCD2(int num1, int num2) {
+        while (num2 != 0) {
+            int tmp = num1 % num2;
+            num1 = num2;
+            num2 = tmp;
+        }
+        return num1;
+    }
+
 
     public static void main(String[] args) {
         int gcd = getGCD(154, 42);
+        int gcd2 = getGCD2(154,42);
         System.out.println("gcd = " + gcd);
+        System.out.println("gcd2 = " + gcd2);
     }
 }
